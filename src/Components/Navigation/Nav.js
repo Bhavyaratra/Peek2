fe6251bc7c63961b7c45fb9a3c73ec6136ab5027
Bhavya Ratra <bhavya.r1220@gmail.com>
2021-04-01 23:33:53 +0530

    first commit

---
+++ src/Components/Navigation/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Name from "./Name";
+import Logo from './Logo';
+export default function Navbar(){
+
+    return(
+    <div className="App-header">
+        <Logo/>
+        <Name/>
+    </div>
+    );
+}
