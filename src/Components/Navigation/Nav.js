4a5b9cc539a6bf4aade505a85aba2a060f682842
Bhavya Ratra <bhavya.r1220@gmail.com>
2021-04-02 23:44:49 +0530

    added navbar css

--- src/Components/Navigation/Nav.js
+++ src/Components/Navigation/Nav.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Name from "./Name";
 import Logo from './Logo';
+import './Navbar.css';
 export default function Navbar(){
 
     return(
-    <div className="App-header">
+    <div className="navbar">
         <Logo/>
         <Name/>
     </div>
