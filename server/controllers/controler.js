4354bcbab1ee2a425bc4f3c0617dedf4a1154b08
Bhavya Ratra <bhavya.r1220@gmail.com>
2021-04-05 14:19:27 +0530

    created notes model

--- server/controllers/controler.js
+++ server/controllers/controler.js
@@ -1,8 +1,24 @@
+const note = require('../models/notes');
 
 const start =(req,res)=>{
     res.send('Starting api page');
 }
 
+const show = (req,res)=>{
+    note.find()
+    .then((result)=>{
+        res.json(result);
+    })
+    .catch((err)=>{
+        res.send('404'+err);
+    })
+}
+
+const save = (req,res)=>{
+    newnote = new note;
+    console.log(newnote);
+}
+
 module.exports = {
-    start,
+    start,show,save,
 }
--- server/router/routes.js
+++ server/router/routes.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const Controller = require('../controllers/controler');
 
 router.get('/',Controller.start);
+router.get('/notes',Controller.show);
+
+
+////////////////////////////////////
+router.post('/notes',Controller.save);
 
 module.exports = router;
 
