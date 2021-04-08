ee668369165d437abb09899c20be0a6f864b2aa4
Bhavya Ratra <bhavya.r1220@gmail.com>
2021-04-08 23:17:23 +0530

    used material ui

--- client/src/Components/Note/AllNotes.js
+++ client/src/Components/Note/AllNotes.js
@@ -1,6 +1,13 @@
 
 import React from 'react';
 import {useEffect,useState} from 'react';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 export const AllNotes = ()=>{
     const [data, setdata] = useState([])
@@ -16,18 +23,48 @@ export const AllNotes = ()=>{
     })
 
     console.log(data)
-    return(<div>
+
+    const useStyles = makeStyles((theme) => ({
+        root: {
+          display: 'Inline',
+          minWidth: 275,
+           
+          boxShadow: '0 3px 5px 2px rgba(22, 21, 21, .3)',
+          marginTop: 10,
+          background: 'rgb(39, 39, 39)',
+          color: 'white',
+        },
+        title: {
+            fontSize: 14,
+          },
+          pos: {
+            marginBottom: 12,
+          },
+      }));
+      const classes = useStyles();
+    return(<div >
         <h1>ALL NOTES</h1>
-        <ul className="list">
-            {data.map((notes)=>(
-                <li className="elements" key={notes.id}>
-                    <h2>{notes.title}</h2>
-                    {notes.content}
-                    <hr/>
-                </li>
-            ))}
+        {/* <ul className="list"> */}
             
-        </ul> 
+            {data.map((notes)=>(
+                <div className="card">
+        <Card className={classes.root}>
+                <CardContent className="elements" key={notes.id}>
+                <Typography variant="h5" component="h2">
+                    {notes.title}
+                    </Typography>
+                <Typography variant="body2" component="p">    
+                    {notes.content}
+                    </Typography>
+                </CardContent>
+                <Button size="small" variant="contained" color="secondary">Delete</Button>
+            </Card>
+            </div>
+            ))}
+
+            
+            
+        {/* </ul>  */}
         </div>)
 
 }
