d8ed49475477f1d6f1ff7f7cd86865b4987115bd
Bhavya Ratra <bhavya.r1220@gmail.com>
2021-04-14 23:02:49 +0530

    created login page

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,33 @@ import './App.css';
 import Navbar from './Components/Navigation/Nav';
 import Note from './Components/Note/Note';
 import {AllNotes} from './Components/Note/AllNotes';
-
+import Login from './Components/Login/Login';
+import {BrowserRouter as Router,
+          Switch,
+          Route,
+          Link}  from 'react-router-dom';
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
 
 function App() {
   return (<>
+       
+        <Router>
         <div className="App"> 
         <Navbar/>
         </div> 
-
-          <Note/>
-          <br/>
-          <AllNotes/>
+        <Switch>
+          <Route path="/login">
           
+            <Login/>
+           
+          </Route>
+          <Route path="/">
+            <Note/>
+            <br/>
+            <AllNotes/>
+          </Route>
+        </Switch>
+        </Router>  
         </>
   );
 }
