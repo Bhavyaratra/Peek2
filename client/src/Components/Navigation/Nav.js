2f7168f00a681d0d0480eb621adeaf07c471d40b
Bhavya Ratra <bhavya.r1220@gmail.com>
2021-04-14 23:02:27 +0530

    added router in nav

--- client/src/Components/Navigation/Nav.js
+++ client/src/Components/Navigation/Nav.js
@@ -2,12 +2,24 @@ import React from "react";
 import Name from "./Name";
 import Logo from './Logo';
 import './Navbar.css';
-export default function Navbar(){
+import Button from '@material-ui/core/Button';
+import {Link}  from 'react-router-dom';
 
+
+
+export default function Navbar(){
+    
     return(
     <div className="navbar">
+        <Link to="/">
         <Logo/>
+        </Link>
         <Name/>
+        <div className="nav-buttons">
+        <Link to="/login">
+            <Button className='nav-button'>Login</Button>
+        </Link>
+        </div>
     </div>
     );
 }
