dd566df6a0e0e7b00c91ea784641745724e68af7
Bhavya Ratra <bhavya.r1220@gmail.com>
2021-04-15 19:49:03 +0530

    changed navbar

--- client/src/Components/Navigation/Nav.js
+++ client/src/Components/Navigation/Nav.js
@@ -2,24 +2,53 @@ import React from "react";
 import Name from "./Name";
 import Logo from './Logo';
 import './Navbar.css';
+import {Link} from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import {Link}  from 'react-router-dom';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
+const useStyles = makeStyles((theme) => ({
+  navbar: {
+    flexGrow: 1,
+    maxHeight: '8hv',
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  navButton: {
+      color: "black",
+      background: "crimson",
+  },
+  appBar:{
+      background: 'black'
+  }
+}));
 
+export default function ButtonAppBar() {
+  const classes = useStyles();
 
-export default function Navbar(){
-    
-    return(
-    <div className="navbar">
-        <Link to="/">
-        <Logo/>
-        </Link>
-        <Name/>
-        <div className="nav-buttons">
-        <Link to="/login">
-            <Button className='nav-button'>Login</Button>
-        </Link>
-        </div>
+  return (
+    <div className={classes.navbar}>
+      <AppBar className={classes.appBar}position="static">
+        <Toolbar>
+          <Link edge="start" className={classes.menuButton} to="/">
+            <Logo/>
+          </Link>
+          <Typography variant="h6" className={classes.title}>
+            Peek 2.0
+          </Typography>
+          <Link to="/login">
+          <Button color="inherit" className={classes.navButton}>Login</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
     </div>
-    );
+  );
 }
