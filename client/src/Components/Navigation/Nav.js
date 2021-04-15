09fc3f4afffad941a305de36e8f7f57fea7c7cdc
Bhavya Ratra <bhavya.r1220@gmail.com>
2021-04-15 19:54:33 +0530

    nav css

--- client/src/Components/Navigation/Nav.js
+++ client/src/Components/Navigation/Nav.js
@@ -1,15 +1,11 @@
 import React from "react";
-import Name from "./Name";
 import Logo from './Logo';
-import './Navbar.css';
 import {Link} from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
   navbar: {
@@ -25,6 +21,9 @@ const useStyles = makeStyles((theme) => ({
   navButton: {
       color: "black",
       background: "crimson",
+      '&:hover': {
+          background: "white"
+      }
   },
   appBar:{
       background: 'black'
