82d44dab95c62a5e7931a93043901e4ed2382f60
Bhavya Ratra <bhavya.r1220@gmail.com>
2021-04-16 10:07:56 +0530

    new logo

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## MERN-Stack minimal Notes making app
 
-<img src="https://i.imgur.com/dTTzb4s.png"/>
+<img src="https://i.imgur.com/ZWAipvt.png"/>
 
 ## Tech used
 ```
--- client/src/Components/Navigation/Logo.js
+++ client/src/Components/Navigation/Logo.js
@@ -3,7 +3,7 @@ import React from "react";
 const Logo = () => {
     return (
         <div>
-    <img src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png" alt="logo"/>
+    <img src="https://i.imgur.com/o258qql.png" width="28px" alt="logo"/>
             
         </div>
     )
