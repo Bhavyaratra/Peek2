dd6c0f63136d8ea533e56eee0d0a83386e78299e
Bhavya Ratra <bhavya.r1220@gmail.com>
2021-04-23 14:56:12 +0530

    removed reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
 import './App.css';
-import React,{createContext,useReducer} from 'react';
+
 import Navbar from './Components/Navigation/Nav';
 import Note from './Components/Note/Note';
 import {AllNotes} from './Components/Note/AllNotes';
 import Login from './Components/Login/Login';
 import Logout from './Components/Logout/Logout';
 import Register from './Components/Register/Register';
-import {initialState,reducer} from '../src/reducer/UseReducer';
+
 import {BrowserRouter as Router,
           Switch,
           Route,
         }  from 'react-router-dom';
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
 
-export const UserContext = createContext();
+
 const App= ()=> {
 
-  const [state, dispatch] = useReducer(reducer, initialState)
+  
   return (<>
 
-        <UserContext.Provider value={{state, dispatch}}>
+       
 
         <Router>
         <div className="App"> 
@@ -51,7 +51,6 @@ const App= ()=> {
           </Route>
         </Switch>
         </Router>  
-        </UserContext.Provider>
         </>
   );
 }
